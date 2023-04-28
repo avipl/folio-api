6c187ee5c31b33cf98510cdd564c9281f32e8a94
Avinash Lakhmawad <avipl1994@gmail.com>
2023-04-27 17:21:44 -0700

    merge cw and personal projs

--- functions/index.js
+++ functions/index.js
@@ -16,6 +16,7 @@ apis.use(cors({origin: true}))
 admin.initializeApp();
 
 const git_token = defineSecret('GIT_PERSONAL_TOKEN')
+const collection_name = 'projs'
 
 function update_readme(git_proj_name){
     return new Promise((resolve, rejects) => { 
@@ -38,6 +39,18 @@ function update_readme(git_proj_name){
     })
 }
 
+function convert_to_perc(langs){
+    let total = 0;
+    Object.keys(langs).forEach((key) => {
+        total += langs[key]
+    })
+    Object.keys(langs).forEach((key) => {
+        langs[key] = parseInt(langs[key]/total * 100)
+    })
+
+    return langs
+}
+
 function update_langs(git_proj_name){
     return new Promise((resolve, rejects) => {
         request({
@@ -50,7 +63,11 @@ function update_langs(git_proj_name){
             }
         }, function(error, response, body){
             if(!error && response.statusCode == 200){
-                resolve(body)
+                let lang = JSON.parse(body)
+                if(Object.keys(lang).length){
+                    lang = convert_to_perc(lang)
+                }
+                resolve(lang)
             }else{
                 functions.logger.error('Error getting languages for project' + git_proj_name, body)
                 rejects(error)
@@ -60,47 +77,43 @@ function update_langs(git_proj_name){
 }
 
 exports.update_proj_details = functions.runWith({ secrets: ['GIT_PERSONAL_TOKEN']}).https.onRequest((req, res) => {
-    if(!('proj_name' in req.query && 'git_proj_name' in req.query && 'proj_type' in req.query)){
-        functions.logger.info('Error while updating project details' + proj_name, err)
-        res.status(400).send('Missing parameter. Required parameters: proj_name, git_proj_name, proj_type')
+    if(!('git_proj_name' in req.query)){
+        functions.logger.info('Error while updating project details' + git_proj_name, err)
+        res.status(400).send('Missing parameter git_proj_name')
     }
-    
-    let proj_name = req.query.proj_name
     let git_proj_name = req.query.git_proj_name
-    let proj_type = req.query.proj_type
 
     Promise.all([update_langs(git_proj_name), update_readme(git_proj_name)]).then(result => {
         let langs = result[0]
         let readme = result[1]
 
         //Update to DB
-        admin.firestore().collection(proj_type).doc(proj_name).set({
+        admin.firestore().collection(collection_name).doc(git_proj_name).update({
             langs: langs, 
             readme: readme, 
-            title: proj_name,
-            pname: proj_name, 
             lu: Timestamp.fromDate(new Date())
         })
 
         res.status(200).json({response: 'okay'})
     }).catch(err => {
-        functions.logger.error('Error while updating project details' + proj_name, err)
+        functions.logger.error('Error while updating project details' + git_proj_name, err)
         res.status(500).send(err)
     })
 })
 
-apis.get('/get_cw_proj', (req, res) => {
-    const result_prom = admin.firestore().collection('cw_proj').get()
+apis.get('/get_projs', (req, res) => {
+    const result_prom = admin.firestore().collection(collection_name).get()
 
     result_prom.then(result =>{
         if(result.size){
             let data = {}
             result.docs.forEach(doc => {
-                let pname = doc.get('pname')
+                let id = doc.get('id')
 
-                data[pname] = {
-                    id: pname,
-                    langs: JSON.parse(doc.get('langs')),
+                data[id] = {
+                    id: id,
+                    langs: doc.get('langs'),
+                    pname: doc.get('pname'),
                     lu: doc.get('lu').toDate(),
                     readme: doc.get('readme'),
                     snap: doc.get('snap'),
@@ -113,34 +126,7 @@ apis.get('/get_cw_proj', (req, res) => {
             res.status(404).json({Msg: 'No project details'});
         }
     }).catch(err => {
-        res.status(500).json({error: 'Error while fetching course projects.'});
-    })
-})
-
-apis.get('/get_personal_proj', (req, res) => {
-    const result_prom = admin.firestore().collection('personal_proj').get()
-
-    result_prom.then(result =>{
-        if(result.size){
-            let data = {}
-            result.docs.forEach(doc => {
-                let pname = doc.get('pname')
-
-                data[pname] = {
-                    id: pname,
-                    langs: doc.get('langs'),
-                    lu: doc.get('lu'),
-                    readme: doc.get('readme'),
-                    snap: doc.get('snap'),
-                    title: doc.get('title')
-                }
-            });
-
-            res.json(data)
-        }else{
-            res.status(404).json({error: 'No project details'});
-        }
-    }).catch(err => {
+        functions.logger.error('Error fetching data ', err)
         res.status(500).json({error: 'Error while fetching course projects.'});
     })
 })
