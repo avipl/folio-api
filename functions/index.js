fbc9d3f7d407f7c00883c4e5c901d177b5119a24
Avinash Lakhmawad <avipl1994@gmail.com>
2023-04-29 16:57:27 -0700

    git webhook integration

--- functions/index.js
+++ functions/index.js
@@ -6,18 +6,19 @@ const express = require("express");
 const cors = require("cors");
 const request = require("request");
 const {Timestamp} = require("firebase-admin/firestore");
-const { rejects } = require("assert");
-const { error } = require("console");
+const crypto = require("crypto")
 
 const apis = express();
-const appCheck = defineBoolean("APP_CHECK");
-apis.use(cors({origin: true}));
+const gitapis = express();
+gitapis.use(cors({origin: true}));
 
-// if(appCheck) {
-//   apis.use(cors({origin: ["avi-portfolio.net", "localhost:5173"]}))
-// } else{
-//   apis.use(cors({origin: true}));
-// }
+const appCheck = defineBoolean("APP_CHECK");
+if(appCheck.value()) {
+  apis.options('/get_projs', cors({origin: "https://avi-portfolio.net"}))
+  apis.use(cors({origin: "https://avi-portfolio.net"}))
+} else{
+  apis.use(cors({origin: true}));
+}
 
 const apiKey = defineString("API_KEY");
 const authDomain = defineString("AUTH_DOMAIN");
@@ -35,6 +36,7 @@ const firebaseConfig = {
 admin.initializeApp(firebaseConfig);
 
 const gitToken = defineSecret("GIT_PERSONAL_TOKEN");
+const signKey = defineString("GIT_HOOK_KEY");
 const collectionName = "projs";
 
 function appCheckVerification(req, res) {
@@ -124,31 +126,63 @@ function update_langs(gitProjName) {
   })
 }
 
-exports.update_proj_details = functions.runWith({ secrets: ['GIT_PERSONAL_TOKEN'] }).https.onRequest((req, res) => {
-  if(!('gitProjName' in req.query)) {
-    res.status(400).send('Missing parameter gitProjName');
-  }
-  let gitProjName = req.query.gitProjName;
-
-  Promise.all([update_langs(gitProjName), update_readme(gitProjName)]).then(result => {
-    let langs = result[0];
-    let readme = result[1];
-
-    //Update to DB
-    admin.firestore().collection(collectionName).doc(gitProjName).update({
-      langs: langs, 
-      readme: readme, 
-      lu: Timestamp.fromDate(new Date())
-    });
-
-    res.status(200).json({response: 'okay'});
-  }).catch(err => {
-    functions.logger.error('Error while updating project details' + gitProjName, err);
-    res.status(500).send(err);
+// Make GitHub call to pull data
+function pullRepoData(gitProjName){
+  return new Promise((resolve, reject) => {
+    Promise.all([update_langs(gitProjName), update_readme(gitProjName)]).then(result => {
+      let langs = result[0];
+      let readme = result[1];
+  
+      //Update to DB
+      admin.firestore().collection(collectionName).doc(gitProjName).update({
+        langs: langs, 
+        readme: readme, 
+        lu: Timestamp.fromDate(new Date())
+      });
+  
+      resolve(true);
+    }).catch(err => {
+      functions.logger.error('Error while updating project details' + gitProjName, err);
+      reject(false);
+    })
   })
-})
+}
+
+function verifySignature(req){
+  let sign = Buffer.from(req.header('X-Hub-Signature-256'), 'utf8');
+
+  let hmac = crypto.createHmac('sha256', signKey.value());
+  let bodySign = Buffer.from(('sha256=' + hmac.update(toString(req.body)).digest('hex')), 'utf8');
+
+  if(bodySign.length != sign.length && !crypto.timingSafeEqual(sign, digest)){
+    return false;
+  }
+
+  return true;
+}
+
+gitapis.post('/update_data', (req, res) => {
+  if(req.header('X-Hub-Signature-256') == null) return res.status(403).send('Signature not found');
+
+  if(verifySignature(req)){
+    // Check if pull request is closed
+    if(req.body.action == 'closed'){
+      pullRepoData(req.body.repository.name).then(succ => {
+        res.status(200).json('okay');
+      }).catch(fail => {
+        res.status(500).json('Failed to update repository information.')
+      });
+    }else{
+      res.status(200).json('Ping response');
+    }
+  }else{
+    res.status(403).json("Signature didn't match.");
+  }
+});
 
 function getProjData(req, res){
+  res.header('Access-Control-Allow-Origin', 'https://avi-portfolio.net');
+
   const result_prom = admin.firestore().collection(collectionName).get();
   result_prom.then(result => {
     if(result.size){
@@ -183,7 +217,6 @@ apis.get('/get_projs', (req, res) => {
       getProjData(req, res);
     }).catch(err => {
       functions.logger.warn('Failed captcha verification.', err)
-      console.log(err)
       res.status(401).send('Verification failed')
     });
   }else{
@@ -191,4 +224,5 @@ apis.get('/get_projs', (req, res) => {
   }
 })
 
+exports.gitapis = functions.runWith({secrets: ['GIT_PERSONAL_TOKEN']}).https.onRequest(gitapis);
 exports.apis = functions.https.onRequest(apis);
