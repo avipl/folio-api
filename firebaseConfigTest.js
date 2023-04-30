7f22afac047b20a35cbda4637f1c1dd3f014412e
Avinash <avipl1994@gmail.com>
2023-04-29 22:09:12 -0700

    Delete firebaseConfigTest.js

--- firebaseConfigTest.js
+++
@@ -1,9 +0,0 @@
-// Your web app's Firebase configuration
-export const firebaseConfig = {
-  apiKey: "AIzaSyCUhJzCFBFfUcGv_gT_BJ4AB6rCpgFRdCY",
-  authDomain: "avi-portfolio-apis.firebaseapp.com",
-  projectId: "avi-portfolio-apis",
-  storageBucket: "avi-portfolio-apis.appspot.com",
-  messagingSenderId: "891226803447",
-  appId: "1:891226803447:web:d0b43fc8ac7c0c920d17f2"
-};
